54f8e8de3ed0db5c2f11f8b1d21ca13e717ade30
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-07-26 23:27:30 +0530

    Privacy

---
+++ pages/Privacy.js
@@ -0,0 +1,163 @@
+import React from "react";
+
+function PrivacyPolicy() {
+  return (
+    <div className="lg:px-[96px] py-[90px] px-6 font-general-sans bg-[#D7F3FF] min-h-screen">
+      <h1 className="text-[32px] font-semibold text-center lg:text-left">
+        Privacy Policy
+      </h1>
+      <br />
+      Last modified: December 29, 2020 <br />
+      <br /> Renovation Brands, LLC This website is made available to you by
+      Renovation Brands, LLC and each of its parent companies, subsidiaries,
+      divisions, affiliates, or other related companies (“Renovation,” “we,” or
+      “us”). The following privacy policy applies to Renovation (“Privacy
+      Policy”) including its websites, and any services offered through its
+      websites and any orders that you place (collectively, the “Services”). At
+      Renovation, we are committed to protecting the privacy and security of
+      information you share with us. This Privacy Policy describes how your
+      personal information is collected, used, and shared when you visit or make
+      a purchase from https://renovationbrands.com and all related websites that
+      link to this Privacy Policy (the “Website”). CHANGES TO OUR PRIVACY POLICY
+      Although most changes are likely to be minor, Renovation may change its
+      Privacy Policy from time to time. Renovation encourages you to frequently
+      check this webpage for any changes to its Privacy Policy. If we make
+      changes, we will notify you by adding a statement to our homepage and in
+      some cases by sending you an email. Your continued access of the Website
+      shall be deemed your conclusive acceptance of any changes to the Privacy
+      Policy. Children Under the Age of 16 Our Website is not intended for
+      children under 16 years of age. No one under age 16 may provide any
+      information to or on the Website. We do not knowingly collect personal
+      information from children under 16. If you are under 16, do not use or
+      provide any information on this Website or through any of its features,
+      make any purchases through the Website, or provide any information about
+      yourself to us, including your name, address, telephone number, email
+      address, or any screen name or user name you may use. If we learn we have
+      collected or received personal information from a child under 16 without
+      verification of parental consent, we will delete that information. If you
+      believe we might have any information from or about a child under 16,
+      please contact us at customerservice@rtacabinetstore.com. PERSONAL
+      INFORMATION WE COLLECT Information You Provide to Us We may collect
+      information from you when you make a purchase or attempt to make a
+      purchase through the Website, subscribe to a newsletter, respond to a
+      survey, fill out a form, enter other information on the Website, or
+      subscribe to our Services. In these instances, you provide us your name,
+      billing address, shipping address, payment information (including credit
+      card numbers, wire transfer information, Affirm transaction details, email
+      address, and phone number. We refer to this information as “Order
+      Information.” If you fill out an online form on this website, we retain
+      the information provided therein to communicate order updates, market and
+      generally communicate unless you decide to opt-out. You may opt out at the
+      bottom of any system-generated email or by sending a message to. When you
+      post content, the information contained in your posing will be stored in
+      our servers. We retain information on your behalf, including but not
+      limited to messages you send, using your account. Information
+      Automatically Collected When you visit the Website, we automatically
+      collect certain information about your device, including information about
+      your web browser, IP address, time zone, and some of the cookies that are
+      installed on your device. Additionally, as you browse the Website, we
+      collect information about the individual web pages or products that you
+      view, what websites or search terms referred you to the Website, and
+      information about how you interact with the Website. We refer to this
+      automatically-collected information as “Device Information.” We collect
+      Device Information using the following technologies: “Cookies” are data
+      files that are placed on your device or computer and often include an
+      anonymous unique identifier. For more information about cookies, and how
+      to disable cookies, visit http://www.allaboutcookies.org. “Log files”
+      track actions occurring on the Website, and collect data including your IP
+      address, browser type, Internet service provider, referring/exit pages,
+      and date/time stamps. “Web beacons,” “tags,” and “pixels” are electronic
+      files used to record information about how you browse the Website.
+      References to “Personal Information” in this Privacy Policy, means Device
+      Information and Order Information.HOW DO WE USE YOUR PERSONAL INFORMATION?
+      We use the Order Information that we collect generally to fulfill any
+      orders placed through the Website (including processing your payment
+      information, arranging for shipping, and providing you with invoices
+      and/or order confirmations). Additionally, we use this Order Information
+      to:Communicate with you; Screen our orders for potential risk or fraud;
+      When in line with the preferences you have shared with us, provide you
+      with information or advertising relating to our products or services; and
+      Share with manufacturing partners for the purposes of fulfilling the
+      order. We use the Device Information that we collect to help us screen for
+      potential risk and fraud (in particular, your IP address), and more
+      generally to improve and optimize our Website (for example, by generating
+      analytics about how our customers browse and interact with the Website,
+      and to assess the success of our marketing and advertising campaigns).
+      DISCLOSURE OF YOUR PERSONAL INFORMATION We disclose your Personal
+      Information with third parties to help us use your Personal Information,
+      as described above. For example, we use Shopify to power our online stores
+      for our various brands – you can read more about how Shopify uses your
+      Personal Information here: https://www.shopify.com/legal/privacy. We also
+      use Google Analytics to help us understand how our customers use the
+      Website--you can read more about how Google uses your Personal Information
+      here: https://www.google.com/intl/en/policies/privacy/. You can also
+      opt-out of Google Analytics here:
+      https://tools.google.com/dlpage/gaoptout. We may disclose your Personal
+      Information with third party service providers to provide you with the
+      Services that we offer; conduct quality assurance testing; facilitate the
+      creation of accounts, and to provide technical support. Finally, we may
+      disclose your Personal Information, if it is believed in good faith that
+      such disclosure is necessary: (i) in connection with any legal
+      investigation; (ii) to comply with relevant laws or to respond to
+      subpoenas or warrants served to Renovation; (iii) to protect or defend the
+      rights or property of Renovation; and/or (iv) to investigate or assist in
+      preventing any violation or potential violation of the law, this Privacy
+      Policy, or our Terms and Conditions, including for billing and collection
+      purposes. DO NOT TRACK Please note that we do not alter our Website’s data
+      collection and use practices when we see a Do Not Track signal from your
+      browser. CHOICES ABOUT HOW WE USE AND DISCLOSE YOUR INFORMATION We strive
+      to provide you with choices regarding the personal information you provide
+      to us. We have created mechanisms to provide you with the following
+      control over your information: Tracking Technologies and Advertising. You
+      can set your browser to refuse all or some browser cookies, or to alert
+      you when cookies are being sent. To learn how you can manage your Flash
+      cookie settings, visit the Flash player settings page on Adobe’s website.
+      If you disable or refuse cookies, please note that some parts of this site
+      may then be inaccessible or not function properly. Disclosure of Your
+      Information for Third-Party Advertising. If you do not want us to share
+      your personal information with unaffiliated or non-agent third parties for
+      promotional purposes, you can opt-out by logging into the Website and
+      adjusting your user preferences in your account profile, checking or
+      unchecking the relevant boxes, or by sending us an email with your request
+      to customerservice@rtacabinetstore.com. Promotional Offers from the
+      Company. If you do not wish to have your email address and contact
+      information used by Renovation to promote our own or third parties’
+      products or services, you can opt-out by logging into the Website and
+      adjusting your user preferences in your account profile, checking or
+      unchecking the relevant boxes, or by sending us an email with your request
+      to customerservice@rtacabinetstore.com. If we have sent you a promotional
+      email, you may send us a return email asking to be omitted from future
+      email distributions. This opt out does not apply to information provided
+      to the Company as a result of a product purchase, warranty registration,
+      product service experience or other transactions. Targeted Advertising. If
+      you do not want us to use information that we collect or that you provide
+      to us to deliver advertisements according to our advertisers’
+      target-audience preferences, you can opt-out by visiting the following
+      links:
+      https://www.facebook.com/settings/?tab=adsGOOGLE;https://www.google.com/settings/ads/anonymous;
+      https://advertise.bingads.microsoft.com/en-us/resources/policies/personalized-
+      ads and submitting a request to customerservice@rtacabinetstore.com.
+      Additionally, you can opt out of some of these services by visiting the
+      Digital Advertising Alliance’s opt-out portal at:
+      http://optout.aboutads.info/. We do not control third parties’ collection
+      or use of your information to serve interest-based advertising. However,
+      these third parties may provide you with ways to choose not to have your
+      information collected or used in this way. You can opt out of receiving
+      targeted ads from members of the Network Advertising Initiative (“NAI”) on
+      the NAI’s website. CALIFORNIA RESIDENTS PRIVACY RIGHTS California Civil
+      Code Section § 1798.83 permits users of our Website that are California
+      residents to request certain information regarding our disclosure of
+      personal information to third parties for their direct marketing purposes.
+      To make such a request, please send an email to or write us at P.O. Box
+      605, Chanhassen, Minnesota, 55317. The supplemental privacy policy for
+      California residents can be found at
+      https://www.rtacabinetstore.com/renovation-brands-privacy-notice-for-california-residents/
+      and supplements the information contained herein. CONTACT US For more
+      information about our privacy practices, if you have questions, or if you
+      would like to make a complaint, please contact us by e-mail at
+      customerservice@rtacabinetstore.com
+    </div>
+  );
+}
+
+export default PrivacyPolicy;
