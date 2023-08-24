07c64acf04f743312e3bc12a4675d61908ef55b2
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-08-25 01:25:57 +0530

    hover state

--- components/Fragments/Realkitchen/index.js
+++ components/Fragments/Realkitchen/index.js
@@ -45,7 +45,7 @@ function Real() {
       <div className="mt-16 flex items-center justify-center space-x-2">
         <button
           onClick={() => setState(state === 0 ? 2 : state - 1)}
-          className="h-[48px] w-[38px] flex items-center justify-center border-[2px] rounded border-[#023E8A]"
+          className="h-[48px] w-[38px] flex items-center justify-center border-[2px] rounded border-[#023E8A]  hover:bg-[#023E8A] hover:text-white"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -56,13 +56,13 @@ function Real() {
           >
             <path
               d="M11 6.75C11.4142 6.75 11.75 6.41421 11.75 6C11.75 5.58579 11.4142 5.25 11 5.25V6.75ZM0.46967 5.46967C0.176777 5.76256 0.176777 6.23744 0.46967 6.53033L5.24264 11.3033C5.53553 11.5962 6.01041 11.5962 6.3033 11.3033C6.59619 11.0104 6.59619 10.5355 6.3033 10.2426L2.06066 6L6.3033 1.75736C6.59619 1.46447 6.59619 0.989593 6.3033 0.696699C6.01041 0.403806 5.53553 0.403806 5.24264 0.696699L0.46967 5.46967ZM11 5.25L1 5.25V6.75L11 6.75V5.25Z"
-              fill="#023E8A"
+              fill="currentColor"
             />
           </svg>
         </button>
         <button
           onClick={() => setState(state === 2 ? 0 : state + 1)}
-          className="h-[48px] w-[38px] flex items-center justify-center border-[2px] rounded border-[#023E8A]"
+          className="h-[48px] w-[38px] flex items-center justify-center border-[2px] rounded border-[#023E8A] hover:bg-[#023E8A] hover:text-white"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -74,7 +74,7 @@ function Real() {
           >
             <path
               d="M11 6.75C11.4142 6.75 11.75 6.41421 11.75 6C11.75 5.58579 11.4142 5.25 11 5.25V6.75ZM0.46967 5.46967C0.176777 5.76256 0.176777 6.23744 0.46967 6.53033L5.24264 11.3033C5.53553 11.5962 6.01041 11.5962 6.3033 11.3033C6.59619 11.0104 6.59619 10.5355 6.3033 10.2426L2.06066 6L6.3033 1.75736C6.59619 1.46447 6.59619 0.989593 6.3033 0.696699C6.01041 0.403806 5.53553 0.403806 5.24264 0.696699L0.46967 5.46967ZM11 5.25L1 5.25V6.75L11 6.75V5.25Z"
-              fill="#023E8A"
+              fill="currentColor"
             />
           </svg>
         </button>
