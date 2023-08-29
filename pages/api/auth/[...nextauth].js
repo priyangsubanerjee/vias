dfdcf81acf80da592453fed9fae1441bb0eb9083
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-08-29 23:52:57 +0530

    secret added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,8 +2,6 @@ import connectDatabase from "@/db/dbConnect";
 import users from "@/db/models/users";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import bcrypt from "bcrypt";
-import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   providers: [
@@ -14,80 +12,10 @@ export const authOptions = {
         timeout: 80000,
       },
     }),
-    // CredentialsProvider({
-    //   name: "credentials",
-    //   credentials: {},
-    //   async authorize(credentials) {
-    //     const { credential, password } = credentials;
-    //     console.log(credential, password);
-    //     await connectDatabase();
-    //     let existingUser = await users.findOne({ credential });
-    //     let match = await bcrypt.compare(password, existingUser.password);
-    //     if (match) {
-    //       return {
-    //         email: existingUser.credential,
-    //       };
-    //     } else {
-    //       return null;
-    //     }
-    //   },
-    // }),
   ],
-  //   callbacks: {
-  //     async session({ session, user, token }) {
-  //       console.log(session);
-  //       if (!session) return;
-
-  //       await connectDatabase();
-  //       let existingUser = null;
-  //       let type = session.user.email.includes("@") ? "email" : "phone";
-
-  //       console.log(type);
-
-  //       if (type === "email") {
-  //         existingUser = await users.findOne({ email: session.user.email });
-  //       } else if (type === "phone") {
-  //         existingUser = await users.findOne({ phone: session.user.email });
-  //       }
-
-  //       console.log(existingUser);
-
-  //       if (existingUser) {
-  //         return {
-  //           ...session,
-  //           user: {
-  //             ...session.user,
-  //             name: existingUser.name,
-  //             email: existingUser.email,
-  //             credential: existingUser.credential,
-  //             id: existingUser._id,
-  //           },
-  //         };
-  //       } else {
-  //         let salts = bcrypt.genSaltSync(10);
-  //         let random6Digitpassword = Math.floor(100000 + Math.random() * 900000);
-  //         let hash = bcrypt.hashSync(random6Digitpassword.toString(), salts);
-  //         let nUser = await users.create({
-  //           name: session.user.name,
-  //           email: session.user.email.includes("@") ? session.user.email : null,
-  //           phone: session.user.email.includes("@") ? null : session.user.email,
-  //           credential: session.user.email,
-  //           password: hash,
-  //           phone: null,
-  //         });
-
-  //         return {
-  //           ...session,
-  //           user: {
-  //             ...session.user,
-  //             name: session.user.name,
-  //             credential: session.user.email,
-  //             id: nUser._id,
-  //           },
-  //         };
-  //       }
-  //     },
-  //   },
+  session: {
+    strategy: "jwt",
+  },
   secret: process.env.SECRET_SALT,
 };
 
