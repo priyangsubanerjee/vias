1fee5db5a6b1a0b393a738acb7c6c318c1cfd854
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-08-30 23:52:55 +0530

    product listing

--- components/Cards/CollectionListing.js
+++ components/Cards/CollectionListing.js
@@ -85,26 +85,26 @@ function CollectionListing({ product }) {
     <div className="border border-[#C3C3C3] bg-white rounded-md">
       <div className="flex items-center justify-center py-3 border-b border-[#C3C3C3]">
         <img
-          src="https://res.cloudinary.com/db9kd4qbi/image/upload/v1692942228/Dtory/vias/image_13_mszddl.png"
+          src={product.image.url}
           className="h-[125px] object-contain"
           alt=""
         />
       </div>
       <div className="px-5 py-4">
         <h2 className="text-[#1B1B1B] font-semibold">{product.name}</h2>
-        <p className="text-[#555] text-sm mt-2">15” W</p>
+        <p className="text-[#555] text-sm mt-2">{product.width}W</p>
         <p className="bg-[#B1C3DB] mt-1 px-2 py-1 rounded-md w-fit text-sm">
-          #HS-B15
+          #{product.tag}
         </p>
         <div className="flex items-center mt-8 text-[#1C7926] font-medium space-x-2">
           <Icon icon="zondicons:checkmark-outline" />
           <span>In Stock</span>
         </div>
         <h2 className="text-[18px] text-[#1B1B1B] font-semibold mt-2">
-          $397.99
+          ${product.price}
         </h2>
         <h2 className="text-[14px] text-[#A3A3A3] font-normal mt-1 line-through">
-          $397.99
+          ${product.price}
         </h2>
         <div className="mt-5 flex justify-between">
           {isAddedToCart ? (
