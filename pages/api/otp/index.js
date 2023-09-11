7a3af0cb371345cb3d64e63a505e548c8275492d
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-09-11 16:56:41 +0530

    await

--- pages/api/otp/index.js
+++ pages/api/otp/index.js
@@ -3,21 +3,21 @@ const accountSid = process.env.TWILIO_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { phone } = JSON.parse(req.body);
   const otp = Math.floor(100000 + Math.random() * 900000);
 
   const salt = bcrypt.genSaltSync(10);
   const hash = bcrypt.hashSync(otp.toString(), salt);
 
-  client.messages
-    .create({
-      body: "hi",
-      from: process.env.OTP_FROM,
-      to: phone,
-    })
-    .then((message) => console.log(message.sid))
-    .done();
+  const message = await client.messages.create({
+    body: `Your OTP is ${otp}`,
+    from: process.env.TWILIO_PHONE_NUMBER,
+    to: phone,
+  });
+
+  console.log(message.sid);
+
   res
     .status(200)
     .json({ success: true, message: "OTP sent successfully", hash });
