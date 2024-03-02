6cd0c20ef9c77ce051b1ad57d65115151a437284
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-03 03:20:05 +0530

    Commented out ShopByCategory component

--- pages/index.js
+++ pages/index.js
@@ -153,7 +153,7 @@ export default function Home() {
       </div>
       <WhatPeopleSay />
       <Real />
-      <ShopByCategory />
+      {/* <ShopByCategory /> */}
       <Youtube />
       {/*
        */}
