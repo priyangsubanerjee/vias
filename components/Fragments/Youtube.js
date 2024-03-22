67fa69cbeb9f74aa0a879e59b418db31bf7a08c1
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-22 23:08:23 +0530

    Update YouTube video source URL

--- components/Fragments/Youtube.js
+++ components/Fragments/Youtube.js
@@ -5,7 +5,7 @@ function Youtube() {
     <div className="lg:px-[96px] lg:pt-[90px] pb-[120px] bg-[#D7F3FF] px-6 py-[50px]">
       <iframe
         className="w-full lg:h-[460px] h-[250px]"
-        src="https://youtu.be/rKGdQfG3rjI"
+        src="https://www.youtube.com/embed/rKGdQfG3rjI"
         title="IKEA Kitchens Design Tour 2022"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
